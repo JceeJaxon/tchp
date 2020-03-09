4f83b628990dfd83fa58cc4166ceb628c8c2835e
JceeJaxon <61695692+JceeJaxon@users.noreply.github.com>
2020-03-09 14:14:49 +0300

    assignment 1-5

---
+++ home.py
@@ -0,0 +1,23 @@
+print('Number 1')
+print('Twinkle, twinkle, little star \n\tHow i wonder what you are \n\t\tFar away up in the sky \n\t\tLike a diamond in the sky, \nTwinkle. twinkle, lttle stars, \n\tHow i wonder what you are! ')
+
+print('Number 2')
+import sys
+print('Python version')
+print(sys.version)
+print('Version info.')
+print(sys.version_info)
+
+print('Number 3')
+print('Current date and time:')
+print('9-3-2020  7:26am')
+
+print('Number 4')
+fname = input('Write your first name:')
+lname = input('Write your last name:')
+print('Hello ' +lname+ ' '  +fname)
+
+print('Number 5')
+from math import pi
+r=float(input('Input the radius of a cirle: '))
+print('The area of the circle with radius ' +str(r)+ ' is ' +str(pi*r))
